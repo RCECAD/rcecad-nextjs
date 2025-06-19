a08c3908908c9393aae60acc92fa9ca84e6feb26
9gods <codingcomelli@gmail.com>
2025-06-18 21:35:04 -0300

    feat: alpha input file

---
+++ src/components/general/input-file.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./button";
+
+export const InputFile = () => {
+	return (
+		<div className="flex flex-col justify-center items-center gap-2 p-4 border-2 border-slate-300 border-dashed rounded-lg">
+			<p>Arraste e solte seu arquivo aqui</p>
+			<p>ou</p>
+			<Button variant="secondary">Escolha um arquivo</Button>
+		</div>
+	);
+};
