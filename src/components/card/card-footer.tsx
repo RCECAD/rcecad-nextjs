a0b3a04840e7b89cfc48c8e83d31638628a72c13
Kaue-Dev <kauemassola900@gmail.com>
2025-06-19 12:16:41 -0300

    update: add justify-between

--- src/components/card/card-footer.tsx
+++ src/components/card/card-footer.tsx
@@ -4,5 +4,5 @@ interface CardFooterProps {
 	children: ReactNode | Array<ReactNode>;
 }
 export const CardFooter = ({ children }: Readonly<CardFooterProps>) => {
-	return <div className="flex w-full justify-end items-center">{children}</div>;
+	return <div className="flex w-full justify-between items-center">{children}</div>;
 };
