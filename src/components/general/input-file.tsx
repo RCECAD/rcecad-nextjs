381f9bfced2d6ebb091cbd0486949b20fbb831c2
Kaue-Dev <kauemassola900@gmail.com>
2025-06-19 12:26:05 -0300

    feat: add min height

--- src/components/general/input-file.tsx
+++ src/components/general/input-file.tsx
@@ -2,7 +2,7 @@ import { Button } from "./button";
 
 export const InputFile = () => {
 	return (
-		<div className="flex flex-col justify-center items-center gap-2 p-4 border-2 border-slate-300 border-dashed rounded-lg">
+		<div className="flex flex-col justify-center items-center gap-2 p-4 border-2 border-slate-300 border-dashed rounded-lg min-h-44">
 			<p>Arraste e solte seu arquivo aqui</p>
 			<p>ou</p>
 			<Button variant="secondary">Escolha um arquivo</Button>
