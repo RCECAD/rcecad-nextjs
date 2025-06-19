998c0de4ee6e72be3bdf7c3c08330d39946aaa84
9gods <codingcomelli@gmail.com>
2025-06-18 21:34:03 -0300

    fix: style bs

--- src/components/card/card-footer.tsx
+++ src/components/card/card-footer.tsx
@@ -4,5 +4,7 @@ interface CardFooterProps {
 	children: ReactNode | Array<ReactNode>;
 }
 export const CardFooter = ({ children }: Readonly<CardFooterProps>) => {
-	return <div className="flex w-full justify-end items-center">{children}</div>;
+	return (
+		<div className="flex w-full justify-end py-3 items-center">{children}</div>
+	);
 };
