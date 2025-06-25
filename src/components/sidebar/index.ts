3454ee1f26be717ce52a73a9f851b5d8b55c76fb
dudaclw <leegabi08@gmail.com>
2025-06-24 23:52:13 -0300

    feat: qlqr coisa

---
+++ src/components/sidebar/index.ts
@@ -0,0 +1,7 @@
+import { DesktopSidebar } from "./desktop-sidebar"
+import { MobileSidebar } from "./mobile-sidebar"
+
+export const SidebarComponent = {
+  DesktopSidebar: DesktopSidebar,
+  MobileSidebar: MobileSidebar,
+}
